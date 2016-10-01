73c9905504515dda0eb9e644b9b09394290b10fe
pedrohcgs <pedrohcgs@gmail.com>
2016-10-01 14:41:33 -0500

    correcting syntax

--- R/pstest.R
+++ R/pstest.R
@@ -30,12 +30,12 @@
 
 
 ###############################################################
-pstest = function(d, pscore, xpscore, model = c("logit", "probit"), nboot = 1000, cores = 1) {
+pstest = function(d, pscore, xpscore, model = "logit", nboot = 1000, cores = 1) {
     n <- length(d)
     xx <- as.matrix(xpscore)
     pscore.fit <- pscore
     uhat <- d - pscore.fit
-    if (model!= "logic" | model!="probit") {
+    if (model!= "logit" | model!="probit") {
       stop("model must be either 'logit' or 'probit'")
     }
     if (model == "logit") {
