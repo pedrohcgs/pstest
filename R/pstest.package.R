82995f6872e298fa6706accd7c2f6a9ead5aff9f
pedrohcgs <pedrohcgs@gmail.com>
2016-10-01 16:16:04 -0500

    improves description

--- R/pstest.package.R
+++ R/pstest.package.R
@@ -2,13 +2,20 @@
 #'
 #'An R package for implementing all the tests proposed in Sant'Anna and
 #'Song (2016), 'Specification Tests for the Propensity Score', available at
-#'Pedro H.C. Sant'Anna webpage, \url{http://sites.google.com/site/pedrohcsantanna/} .
-#'
-#'In short, this package implements bootstrapped based Kolmogorov-Smirnov
+#'Pedro H.C. Sant'Anna webpage, \url{http://sites.google.com/site/pedrohcsantanna/}.
+#'In short, this package implements bootstrapped Kolmogorov-Smirnov
 #'and Cramer-von Mises type tests for parametric propensity score models with
 #'either logistic ('logit'), or standard normal ('probit') link function.
 #'
-#' The module was written by Pedro H.C. Sant'Anna (Vanderbilt University) and Xiaojun Song (Peking University).
+#'
+#'The tests do not suffer from the "curse of dimensionality" when the vector of covariates
+#'is of high-dimensionality, are fully data-driven, do not require tuning parameters
+#'such as bandwidths, and are able to detect a broad class of local alternatives converging
+#'to the null at the parametric rate.
+#'
+#'
+#' The module was written by Pedro H.C. Sant'Anna (Vanderbilt University) and
+#' Xiaojun Song (Peking University).
 #'
 #' A help file with examples will be made available in the near future.
 #'
