529556c71ff6e1c7f659761e7de6e505427f4720
pedrohcgs <pedrohcgs@gmail.com>
2016-11-20 19:19:44 -0600

    Output

--- R/print.pstest.R
+++ R/print.pstest.R
@@ -1,1 +1,26 @@
+#' @title Print
+#'
+#' @description Prints a Summary pstest Object
+#'
+#' @param x A pstest object
+#' @param ... Other params (required as generic function, but not used)
+#'
+#' @export
+# Define new print function
+print.pstest <- function(x, ...){
+  cat("Call:\n")
+  print(x$call)
+  ks.mat <- data.frame(x$kstest, x$pvks)
+  colnames(ks.mat) <- c("Test statistic", "Bootstrapped P-value")
+  rownames(ks.mat) <- c("")
 
+  cvm.mat <- data.frame(x$cvmtest, x$pvcvm)
+  colnames(cvm.mat) <- c("Test statistic", "Bootstrapped P-value")
+  rownames(cvm.mat) <- c("")
+
+  cat("\n Kolmogorov-Smirnov test:\n")
+  print(ks.mat)
+
+  cat("\n Cramer-von Mises test:\n")
+  print(cvm.mat)
+}
--- R/pstest.R
+++ R/pstest.R
@@ -341,22 +341,3 @@ pstest = function(d, pscore, xpscore, model = c("logit", "probit"),
     # return the list
     return(ret)
 }
-
-# Define new print function
-print.pstest <- function(x, ...){
-  cat("Call:\n")
-  print(x$call)
-  ks.mat <- data.frame(x$kstest, x$pvks)
-  colnames(ks.mat) <- c("Test statistic", "Bootstrapped P-value")
-  rownames(ks.mat) <- c("")
-
-  cvm.mat <- data.frame(x$cvmtest, x$pvcvm)
-  colnames(cvm.mat) <- c("Test statistic", "Bootstrapped P-value")
-  rownames(cvm.mat) <- c("")
-
-  cat("\n Kolmogorov-Smirnov test:\n")
-  print(ks.mat)
-
-  cat("\n Cramer-von Mises test:\n")
-  print(cvm.mat)
-}
