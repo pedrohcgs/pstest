5c3ae9593b4943dce5235d67e145b10fa6fde79e
pedrohcgs <pedrohcgs@gmail.com>
2016-11-20 20:55:43 -0600

    Output

--- R/print.pstest.R
+++ R/print.pstest.R
@@ -12,11 +12,11 @@ print.pstest <- function(x, ...){
     #-----------------------------------------------------------------------------
     # Preliminaries
     # Weight function used
-    if(x$argu$w == "ind")       ww <- "Indicator function, w(q,u) = 1(q<=u)"
-    if(x$argu$w == "exp")       ww <- "Exponential function, w(q,u) = exp(qu)"
-    if(x$argu$w == "logistic")  ww <- "Logistic function, w(q,u) = 1/[1+exp(-qu)]"
-    if(x$argu$w == "sin")       ww <- "Sine function, w(q,u) = sin(qu)"
-    if(x$argu$w == "sincos")    ww <- "Sine and cosine function, w(q,u) = sin(qu)+ cos(qu)"
+    if(x$argu$w == "ind")       ww <- "Indicator function: w(q,u) = 1(q<=u)"
+    if(x$argu$w == "exp")       ww <- "Exponential function: w(q,u) = exp(qu)"
+    if(x$argu$w == "logistic")  ww <- "Logistic function: w(q,u) = 1/[1+exp(-qu)]"
+    if(x$argu$w == "sin")       ww <- "Sine function: w(q,u) = sin(qu)"
+    if(x$argu$w == "sincos")    ww <- "Sine and cosine function: w(q,u) = sin(qu)+ cos(qu)"
 
     #Creat parameters for the Table
     header <- c("", "Test statistic", "Bootstrapped P-value")
